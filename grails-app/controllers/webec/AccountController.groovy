ceec308636dfe07d926335b2bab1b64d8fc82492
Nicolas Kalousek <nicolas.zy@gmail.com>
2019-04-28 16:10:12 +0200

    added view of books with option to edit and add books in database

---
+++ grails-app/controllers/webec/AccountController.groovy
@@ -0,0 +1,23 @@
+package webec
+
+import grails.validation.Validateable
+import mvc.SecRole
+import mvc.SecUser
+import mvc.SecUserSecRole
+
+class AccountController {
+
+    def createUsers(String userName, String password){
+
+        SecUser nicolas = new SecUser(username: userName, password: password).save(flush:true)
+        SecRole admin = new SecRole(authority: SecRole.ROLE_ADMIN).save(flush:true)
+        new SecUserSecRole(secUser: nicolas, secRole: admin).save(flush:true)
+
+        render view: 'accounts', model: 'AccountModel'
+    }
+}
+
+class AccountModel implements Validateable{
+
+}
+
--- grails-app/controllers/webec/MyBooksController.groovy
+++ grails-app/controllers/webec/MyBooksController.groovy
@@ -1,24 +1,22 @@
 package webec
 
 import grails.validation.Validateable
+import mvc.Book
 import mvc.SecRole
 import mvc.SecUser
 import mvc.SecUserSecRole
 
 class MyBooksController {
 
-    def createUsers(String userName, String password){
-
-        SecUser nicolas = new SecUser(username: userName, password: password).save(flush:true)
-        SecRole admin = new SecRole(authority: SecRole.ROLE_ADMIN).save(flush:true)
-        new SecUserSecRole(secUser: nicolas, secRole: admin).save(flush:true)
-
-        render view: 'myBooks', model: 'myBooksModel'
+    def index() {
+        def books = Book.list()
+        render view: 'MyBooks', model: [books:books]
     }
+
+
 }
 
 class myBooksModel implements Validateable{
-
 
 }
 
